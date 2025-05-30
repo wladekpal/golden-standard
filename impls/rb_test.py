53dcadeeef877f41b14d054583defb0066e13489
Michał Bortkiewicz <michalbortkiewicz8@gmail.com>
2025-05-30 11:35:50 +0200

    chore: refactor

--- impls/rb_test.py
+++ impls/rb_test.py
@@ -1,28 +1,30 @@
 import os
 
-from config import ROOT_DIR
-from rb import TrajectoryUniformSamplingQueue, flatten_batch, jit_wrap
-
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
 os.environ['CUDA_VISIBLE_DEVICES'] = '4'
 
 import jax
 import jax.numpy as jnp
 import xminigrid
-from xminigrid.wrappers import GymAutoResetWrapper
 from benchmark_test import TimeStepNew, build_benchmark
 from absl import app, flags
 from ml_collections import config_flags
 
+from agents import agents
+from rb import TrajectoryUniformSamplingQueue, flatten_batch, jit_wrap
+from config import ROOT_DIR
 
+# Environment parameters
 VIEW_SIZE = 3
 env_name = 'MiniGrid-EmptyRandom-8x8'
+
+# Random seed TODO: Need to make sure that latter everything is correctly seeded
 key = jax.random.PRNGKey(0)
 buffer_key, reset_key = jax.random.split(key, 2)
+
+# Just to create a dummy timestep
 env, env_params = xminigrid.make(env_name)
 env_params = env_params.replace(view_size=VIEW_SIZE)
-env = GymAutoResetWrapper(env)
-
 timestep = jax.jit(env.reset)(env_params, reset_key)
 timestep = TimeStepNew(
     state=timestep.state,
@@ -32,10 +34,10 @@ timestep = TimeStepNew(
     observation=timestep.observation,
     action=jnp.zeros((1,), dtype=jnp.int32),
 )
-
 print(timestep.observation.shape)
 print(timestep.state.step_num)
 
+# Create a replay buffer
 replay_buffer = jit_wrap(
     TrajectoryUniformSamplingQueue(
         max_replay_size=100,
@@ -46,28 +48,19 @@ replay_buffer = jit_wrap(
     )
 )
 buffer_state = jax.jit(replay_buffer.init)(buffer_key)
-
 print(replay_buffer._data_shape)
 
 
-
+# Create a benchmark function and get a batch of timesteps
 benchmark_fn = build_benchmark(env_name, 256, 50, view_size=VIEW_SIZE)
-
-# TypeError: Cannot determine dtype of key<fry> while using key = jax.random.key(0)
-key = jax.random.PRNGKey(0)
 env_step, timesteps_all = benchmark_fn(key)
 print(f"timesteps_all.action.shape: {timesteps_all.action.shape}")
 
+# RB operations
 buffer_state = replay_buffer.insert(buffer_state, timesteps_all)
-
 buffer_state, transitions = replay_buffer.sample(buffer_state) 
 
-
-
-
-buffer_state, transitions = replay_buffer.sample(buffer_state)
-
-# process transitions for training
+# Process transitions for training
 batch_keys = jax.random.split(buffer_state.key, transitions.observation.shape[0])
 print(f"batch_keys: {batch_keys.shape}")
 state, action, goal = jax.vmap(flatten_batch, in_axes=(None, 0, 0))((0.99, 171, 0), transitions, batch_keys)
@@ -75,14 +68,15 @@ print(f"state.shape: {state.shape}")
 print(f"action.shape: {action.shape}")
 print(f"goal.shape: {goal.shape}")
 
+# Take only first timestep (original state shape (256-BS, 9-T, 3-H, 3-W, 2-C))
+state, action, goal = state[:,0].reshape(state.shape[0], -1), action[:,0].reshape(action.shape[0]), goal[:,0].reshape(goal.shape[0], -1) 
 
-state, action, goal = state[:,0].reshape(state.shape[0], -1), action[:,0].reshape(action.shape[0]), goal[:,0].reshape(goal.shape[0], -1) # Take only first timestep (original state shape (256, 9, 3, 3, 2))
-
+# Define the agent
 FLAGS = flags.FLAGS
 flags.DEFINE_integer('seed', 0, 'Random seed.')
 
 config_flags.DEFINE_config_file('agent', ROOT_DIR + '/agents/crl.py', lock_config=False)
-from agents import agents
+
 
 def main(_):
 # Agent
