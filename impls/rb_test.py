d7453c3f3b3992197adafe6d4f726758a6b840a1
Michał Bortkiewicz <michalbortkiewicz8@gmail.com>
2025-06-02 20:59:17 +0200

    fix: actor creation - correct number of actions

--- impls/rb_test.py
+++ impls/rb_test.py
@@ -1,15 +1,15 @@
 import os
 
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   
-os.environ['CUDA_VISIBLE_DEVICES'] = '4'
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 import jax
 import jax.numpy as jnp
 import xminigrid
-from data_collection import TimeStepNew, build_benchmark
+from data_collection import TimeStepNew, build_benchmark, collect_data
 from absl import app, flags
 from ml_collections import config_flags
-
+from xminigrid.core.constants import NUM_ACTIONS
 from agents import agents
 from rb import TrajectoryUniformSamplingQueue, flatten_batch, jit_wrap
 from config import ROOT_DIR
@@ -68,6 +68,8 @@ print(f"state.shape: {state.shape}")
 print(f"action.shape: {action.shape}")
 print(f"goal.shape: {goal.shape}")
 
+print(f"transitions.reward min, max: {transitions.reward.min()}, {transitions.reward.max()}")
+
 # Take only first timestep (original state shape (256-BS, 9-T, 3-H, 3-W, 2-C))
 state, action, goal = state[:,0].reshape(state.shape[0], -1), action[:,0].reshape(action.shape[0]), goal[:,0].reshape(goal.shape[0], -1) 
 
@@ -85,7 +87,7 @@ def main(_):
     agent_class = agents[config['agent_name']]
     example_batch = {
         'observations':timestep.observation.reshape(1, -1),  # Add batch dimension
-        'actions': jnp.zeros((1,), dtype=jnp.int32),  # Single action for batch size 1
+        'actions': jnp.ones((1,), dtype=jnp.int32) * NUM_ACTIONS, # TODO: make sure it should be the maximal value of action space  # Single action for batch size 1
         # 'value_goals': timestep.state.goal_encoding.reshape(1, -1),
         # 'actor_goals': timestep.state.goal_encoding.reshape(1, -1),
         'value_goals': timestep.observation[:,timestep.observation.shape[1]//2,:].reshape(1, -1),
@@ -111,8 +113,22 @@ def main(_):
         'actor_goals':goal,
         'terminals':jnp.zeros((state.shape[0],), dtype=jnp.int32), # TODO: check if this is correct
     }
+    print(f"agent.sample_actions(valid_batch['observations'], valid_batch['value_goals']): {agent.sample_actions(valid_batch['observations'], valid_batch['value_goals'], seed=key)}")
     agent, update_info = agent.update(valid_batch)
-    # print(f"update_info: {update_info}")
+    
+    print(f"update_info: {update_info}")
+
+    exemplary_goals = jnp.array([6,2,2,6,-1,-1])
+    exemplary_goals = exemplary_goals.reshape(1, -1)
+    exemplary_goals = jnp.tile(exemplary_goals, (256, 1))
+    print(f"exemplary_goals.shape: {exemplary_goals.shape}")
+
+    collect_data_fn = collect_data(env_name, 256, agent, exemplary_goals, 50, view_size=VIEW_SIZE)
+    env_step, timesteps_all = collect_data_fn(key)
+
+    print(f"timesteps_all.reward min, max: {timesteps_all.reward.min()}, {timesteps_all.reward.max()}")
+    # Hmm, seems like it's working.
+
 
 
 
