f155dcad2c2dd0ae5ae45d5ae6cc70ca827ed9c5
Michał Bortkiewicz <michalbortkiewicz8@gmail.com>
2025-08-11 22:19:12 +0200

    fix: gifs

--- src/envs/block_moving_env.py
+++ src/envs/block_moving_env.py
@@ -616,44 +616,42 @@ class BoxPushingEnv:
         return dummy_timestep
 
     @staticmethod
-    def animate(axs, timesteps, frame, img_prefix='assets'):
+    def animate(ax, timesteps, frame, img_prefix='assets'):
+        ax.clear()
+        grid_state = timesteps.grid[0, frame]
+        action = timesteps.action[0, frame]
+        reward = timesteps.reward[0, frame]
 
-        for env_idx, ax in enumerate(axs):
-            ax.clear()
-            grid_state = timesteps.grid[env_idx, frame]
-            action = timesteps.action[env_idx, frame]
-            reward = timesteps.reward[env_idx, frame]
+        # Create color mapping for grid states
+        imgs = {
+            0: 'floor.png',                                  # EMPTY
+            1: 'box.png',                                    # BOX
+            2: 'box_target.png',                             # TARGET
+            3: 'agent.png',                                  # AGENT
+            4: 'agent_carrying_box.png',                     # AGENT_CARRYING_BOX
+            5: 'agent_on_box.png',                           # AGENT_ON_BOX
+            6: 'agent_on_target.png',                        # AGENT_ON_TARGET
+            7: 'agent_on_target_carrying_box.png',           # AGENT_ON_TARGET_CARRYING_BOX
+            8: 'agent_on_target_with_box.png',               # AGENT_ON_TARGET_WITH_BOX
+            9: 'agent_on_target_with_box_carrying_box.png',  # AGENT_ON_TARGET_WITH_BOX_CARRYING_BOX
+            10: 'box_on_target.png',                         # BOX_ON_TARGET
+            11: 'agent_on_box_carrying_box.png'              # AGENT_ON_BOX_CARRYING_BOX
+        }
 
-            # Create color mapping for grid states
-            imgs = {
-                0: 'floor.png',                                  # EMPTY
-                1: 'box.png',                                    # BOX
-                2: 'box_target.png',                             # TARGET
-                3: 'agent.png',                                  # AGENT
-                4: 'agent_carrying_box.png',                     # AGENT_CARRYING_BOX
-                5: 'agent_on_box.png',                           # AGENT_ON_BOX
-                6: 'agent_on_target.png',                        # AGENT_ON_TARGET
-                7: 'agent_on_target_carrying_box.png',           # AGENT_ON_TARGET_CARRYING_BOX
-                8: 'agent_on_target_with_box.png',               # AGENT_ON_TARGET_WITH_BOX
-                9: 'agent_on_target_with_box_carrying_box.png',  # AGENT_ON_TARGET_WITH_BOX_CARRYING_BOX
-                10: 'box_on_target.png',                         # BOX_ON_TARGET
-                11: 'agent_on_box_carrying_box.png'              # AGENT_ON_BOX_CARRYING_BOX
-            }
-
-            # Plot grid
-            for i in range(grid_state.shape[0]):
-                for j in range(grid_state.shape[1]):
-                    img_name = imgs[int(grid_state[i, j])]
-                    img_path = os.path.join(img_prefix, img_name)
-                    img = matplotlib.image.imread(img_path)
-                    ax.imshow(img, extent = [i+1, i, j+1, j])
+        # Plot grid
+        for i in range(grid_state.shape[0]):
+            for j in range(grid_state.shape[1]):
+                img_name = imgs[int(grid_state[i, j])]
+                img_path = os.path.join(img_prefix, img_name)
+                img = matplotlib.image.imread(img_path)
+                ax.imshow(img, extent = [i+1, i, j+1, j])
 
 
-            ax.set_xlim(0, grid_state.shape[1])
-            ax.set_ylim(0, grid_state.shape[0])
-            ax.set_title(f'Step {frame} | Action: {action} | Reward: {reward:.2f}')
-            ax.set_aspect('equal')
-            ax.invert_yaxis()
+        ax.set_xlim(0, grid_state.shape[1])
+        ax.set_ylim(0, grid_state.shape[0])
+        ax.set_title(f'Step {frame} | Action: {action} | Reward: {reward:.2f}')
+        ax.set_aspect('equal')
+        ax.invert_yaxis()
 
 
 class Wrapper(BoxPushingEnv):
