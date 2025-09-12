5f165b17675b2ba3765dd717d7ee8ef2c46a932b
wladekpal <wladek.palucki@gmail.com>
2025-09-12 12:15:52 +0200

    Fixed epsilon_greedy sampling error

--- src/impls/agents/crl_search.py
+++ src/impls/agents/crl_search.py
@@ -164,14 +164,16 @@ class CRLSearchAgent(flax.struct.PyTreeNode):
         def value_transform(x):
             return jnp.log(jnp.maximum(x, 1e-6))
         
+
+        all_actions = jnp.tile(jnp.arange(6), (observations.shape[0], 1))  # B x 6
+        qs = jax.lax.stop_gradient(jax.vmap(self.network.select('critic'), in_axes=(None, None, 1))(observations, goals, all_actions)) # 6 x 2 x B
+        qs = qs.mean(axis=1) # 6 x B
+        qs = value_transform(qs)
+        qs = qs.transpose(1, 0) # B x 6
+        
         if self.config['action_sampling'] in ['softmax', 'norm_softmax']:
             # Use critic to get Q-values (use first/ensemble as appropriate). Prefer the minimum head for conservative action,
             # or average — here we average the two heads and pick argmax.
-            all_actions = jnp.tile(jnp.arange(6), (observations.shape[0], 1))  # B x 6
-            qs = jax.lax.stop_gradient(jax.vmap(self.network.select('critic'), in_axes=(None, None, 1))(observations, goals, all_actions)) # 6 x 2 x B
-            qs = qs.mean(axis=1) # 6 x B
-            qs = value_transform(qs)
-            qs = qs.transpose(1, 0) # B x 6
 
             if self.config['action_sampling'] == 'norm_softmax':
                 qs = (qs - qs.mean(axis=1, keepdims=True)) / jnp.maximum(1e-6, qs.std(axis=1, keepdims=True))  # Normalize logits.
