1b9e41804f0a8ff92907c3fc72e5f6740266e298
Michał Bortkiewicz <michalbortkiewicz8@gmail.com>
2025-09-18 18:17:48 +0200

    fix: target temperature and moveable boxes eval

--- src/train.py
+++ src/train.py
@@ -254,7 +254,7 @@ def evaluate_agent(agent, key, jitted_create_batch, epoch, config):
         eval_names_suff.append("_special")
 
     if config.exp.eval_different_box_numbers:
-        for number_of_boxes in range(1, 12, 2):
+        for number_of_boxes in [config.env.number_of_boxes_max]:
             new_config = copy.deepcopy(config)
             new_config.env = dataclasses.replace(
                 new_config.env,
