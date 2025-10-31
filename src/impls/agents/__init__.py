461145136884dd1888c309e5a63d2b9314c54758
wladekpal <wladek.palucki@gmail.com>
2025-10-31 20:10:43 +0100

    Removed bad imports

--- src/impls/agents/__init__.py
+++ src/impls/agents/__init__.py
@@ -5,8 +5,6 @@ from impls.agents.dqn import GCDQNAgent
 from impls.agents.gcbc import GCBCAgent
 from impls.agents.gciql import GCIQLAgent
 from impls.agents.gciql_search import GCIQLSearchAgent
-from impls.agents.gcivl import GCIVLAgent
-from impls.agents.hiql import HIQLAgent
 from impls.agents.qrl import QRLAgent
 from impls.agents.sac import SACAgent
 import ml_collections
@@ -15,8 +13,6 @@ agents = dict(
     crl=CRLAgent,
     gcbc=GCBCAgent,
     gciql=GCIQLAgent,
-    gcivl=GCIVLAgent,
-    hiql=HIQLAgent,
     qrl=QRLAgent,
     sac=SACAgent,
 )
